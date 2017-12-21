7fe4824fe5cb40984f2ca1563021091358ac8691
Unknown <anobregaalves@hotmail.com>
2017-12-21 09:55:31 +0000

    ..

--- app/Compra.php
+++ app/Compra.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compra extends Model
 {
-    //
+    public function user()
+    {
+    	return $this->belongsTo(App\User);
+
+    }
+
+    public function carrosComprados()
+    {
+    	return $this->hasMany(App\CarroComprado);
+    }
 }
